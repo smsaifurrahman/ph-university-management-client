e3fcc275ad2ad1c2f17b8d8ffd349c38680fabc6
smsaifurrahman <smsaifur525@gmail.com>
2025-01-16 11:58:31 +0100

    AcademicSemester filter is implemented

--- src/pages/admin/academicManagement/AcademicSemester.tsx
+++ src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -1,16 +1,23 @@
 /** @format */
 
 import { useGetAllSemesterQuery } from "../../../redux/features/admin/academicManagement.api";
-import React from "react";
 import { Table } from "antd";
 import type { TableColumnsType, TableProps } from "antd";
+import { TAcademicSemester } from "../../../types/academicManagement.type";
+import { useState } from "react";
+
+export type TTableData = Pick<
+   TAcademicSemester,
+   "_id" | "name" | "year" | "startMonth" | "endMonth"
+>;
 
 const AcademicSemester = () => {
-   const { data: semesterData } = useGetAllSemesterQuery(undefined);
+   const [params, setParams] = useState([]);
+   const { data: semesterData } = useGetAllSemesterQuery(params);
 
    const tableData = semesterData?.data?.map(
       ({ _id, name, startMonth, endMonth, year }) => ({
-         _id,
+         key: _id,
          name,
          startMonth,
          endMonth,
@@ -18,98 +25,84 @@ const AcademicSemester = () => {
       })
    );
 
-   interface DataType {
-      key: React.Key;
-      name: string;
-      age: number;
-      address: string;
-   }
-
-   const columns: TableColumnsType<DataType> = [
+   const columns: TableColumnsType<TTableData> = [
       {
          title: "Name",
+         key: "name",
          dataIndex: "name",
-         showSorterTooltip: { target: "full-header" },
          filters: [
             {
-               text: "Joe",
-               value: "Joe",
+               text: "Autumn",
+               value: "Autumn",
             },
             {
-               text: "Jim",
-               value: "Jim",
+               text: "Fall",
+               value: "Fall",
             },
             {
-               text: "Submenu",
-               value: "Submenu",
-               children: [
-                  {
-                     text: "Green",
-                     value: "Green",
-                  },
-                  {
-                     text: "Black",
-                     value: "Black",
-                  },
-               ],
+               text: "Summer",
+               value: "Summer",
             },
          ],
-        
       },
       {
          title: "Year",
+         key: "year",
          dataIndex: "year",
-         
+         filters: [
+            {
+               text: "2025",
+               value: "2025",
+            },
+            {
+               text: "2026",
+               value: "2026",
+            },
+            {
+               text: "2027",
+               value: "2027",
+            },
+         ],
       },
       {
          title: "Start Month",
+         key: "startMoth",
          dataIndex: "startMonth",
-        
       },
       {
          title: "End Month",
+         key: "endMonth",
          dataIndex: "endMonth",
-        
       },
    ];
 
-  //  const data = [
-  //     {
-  //        key: "1",
-  //        name: "John Brown",
-  //        age: 32,
-  //        address: "New York No. 1 Lake Park",
-  //     },
-  //     {
-  //        key: "2",
-  //        name: "Jim Green",
-  //        age: 42,
-  //        address: "London No. 1 Lake Park",
-  //     },
-  //     {
-  //        key: "3",
-  //        name: "Joe Black",
-  //        age: 32,
-  //        address: "Sydney No. 1 Lake Park",
-  //     },
-  //     {
-  //        key: "4",
-  //        name: "Jim Red",
-  //        age: 32,
-  //        address: "London No. 2 Lake Park",
-  //     },
-  //  ];
-
-   const onChange: TableProps<DataType>["onChange"] = (
+   const onChange: TableProps<TTableData>["onChange"] = (
       pagination,
       filters,
       sorter,
       extra
    ) => {
-      console.log("params", pagination, filters, sorter, extra);
+      if (extra.action === "filter") {
+         const queryParams = [];
+         filters.name?.forEach((item) => {
+            queryParams.push({
+               name: "name",
+               value: item,
+            });
+         });
+         filters.year?.forEach((item) => {
+            queryParams.push({
+               name: "year",
+               value: item,
+            });
+         });
+         setParams(queryParams)
+         console.log(queryParams);
+      }
+      // console.log({ filters, extra });
    };
    return (
-      <Table<DataType>
+      <Table<TTableData>
          columns={columns}
          dataSource={tableData}
          onChange={onChange}
--- src/redux/features/admin/academicManagement.api.ts
+++ src/redux/features/admin/academicManagement.api.ts
@@ -1,30 +1,44 @@
+/** @format */
+
+import { TResponseRedux } from "./../../../types/global";
+
 import { baseApi } from "../../api/baseApi";
+import { TAcademicSemester } from "../../../types/academicManagement.type";
 
 const academicManagementApi = baseApi.injectEndpoints({
-    endpoints: (builder) => ({
-        getAllSemester: builder.query({
-           query: () => ({
-              url: "/academic-semesters",
-              method: "GET",
-            
-           }),
-           transformResponse: (response) => {
-            console.log('inside redux', response);
+   endpoints: (builder) => ({
+      getAllSemester: builder.query({
+         query: (agrs) => {
+            const params = new URLSearchParams();
+
+            if (agrs) {
+               agrs.forEach((item) => {
+                  params.append(item.name, item.value);
+               });
+            }
+            return {
+               url: "/academic-semesters",
+               method: "GET",
+               params: params,
+            };
+         },
+         transformResponse: (response: TResponseRedux<TAcademicSemester[]>) => {
+            console.log("inside redux", response);
             return {
                data: response.data,
                meta: response.meta,
-            }
-           }
-        }),
-        addAcademicSemester: builder.mutation({
-           query: (data) => ({
-              url: "/academic-semesters/create-academic-semester",
-              method: "POST",
-              body: data
-            
-           }),
-        }),
-     }),
-})
+            };
+         },
+      }),
+      addAcademicSemester: builder.mutation({
+         query: (data) => ({
+            url: "/academic-semesters/create-academic-semester",
+            method: "POST",
+            body: data,
+         }),
+      }),
+   }),
+});
 
-export const {useGetAllSemesterQuery, useAddAcademicSemesterMutation} = academicManagementApi
+export const { useGetAllSemesterQuery, useAddAcademicSemesterMutation } =
+   academicManagementApi;
